6c9e0454be3190ec41cbc2b7ed40937826f49fc8
Thor Grimes <thools0731@staden.gotheborg.net>
2024-12-10 10:48:58 +0100

    Commit December 10
    Ny font och gjorde kollision logik

--- Game1.cs
+++ Game1.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+
 namespace Spel_Projekt_Thor_Grimes
 {
     public class Game1 : Game
@@ -17,7 +18,7 @@ namespace Spel_Projekt_Thor_Grimes
         SpriteFont Font;
         List<Rectangle> Walls = new List<Rectangle>();
         int gamestate = 0; // 0 = main menu, 1 = level selector, 2 = in game, 3 = level creator (if i have time)
-        bool debug = false;
+        bool debug = true;
         bool rel = false;
         Vector2 mousepos;
         List<Rectangle> rectangles = new List<Rectangle>();
@@ -31,8 +32,8 @@ namespace Spel_Projekt_Thor_Grimes
         List<Rectangle> LSRectangle = new List<Rectangle>();
         Dictionary<string, Vector2> LSText = new Dictionary<string, Vector2>();
         int level;
-        Dictionary<int, Vector2> startpos = new Dictionary<int, Vector2>(); // to be removed
         Dictionary<int, Dictionary<int, Vector2>> startingpos = new Dictionary<int, Dictionary<int, Vector2>>(); // first int = level, 2nd int = player nr vector2 is pos
+        Dictionary<int, List<Rectangle>> levellistrectangles = new Dictionary<int, List<Rectangle>>(); // int = level, rectangle = rectangle
 
         public Game1()
         {
@@ -52,11 +53,19 @@ namespace Spel_Projekt_Thor_Grimes
             LSText.Add("Back", new Vector2(55, 412));
             LSRectangle.Add(new Rectangle(25, 100, 100, 35));
             LSText.Add("Level 1", new Vector2(52, 112));
+            Dictionary<int, Vector2> startpos = new Dictionary<int, Vector2>();
             startpos.Add(0, new Vector2(100, 400));
             startpos.Add(1, new Vector2(200, 400));
             startpos.Add(2, new Vector2(300, 400));
             startpos.Add(3, new Vector2(400, 400));
             startingpos.Add(1, startpos);
+            List<Rectangle> l = new List<Rectangle>();
+            Rectangle r = new Rectangle(0, 0, 100, 500);
+            l.Add(r);
+            r = new Rectangle(0, 0, 800, 100);
+            l.Add(r);
+            levellistrectangles.Add(1, l);
+
             base.Initialize();
         }
 
@@ -85,6 +94,7 @@ namespace Spel_Projekt_Thor_Grimes
             {
                 LevelSelectorUpdate();
             }
+
             else if (gamestate == 2)
             {
                 GameUpdate();
@@ -158,7 +168,7 @@ namespace Spel_Projekt_Thor_Grimes
                             else if (text.Contains("Level"))
                             {
                                 string[] strings = text.Split(' ');
-                                int level = int.Parse(strings[1]);
+                                level = int.Parse(strings[1]);
                                 InitializeLevel();
                                 gamestate++;
                             }
@@ -172,15 +182,63 @@ namespace Spel_Projekt_Thor_Grimes
             }
             void GameUpdate()
             {
-                
+                for (int i = 0; i < 4; i++)
+                {
+                    player[i].X += (int)velo[i].X;
+                    player[i].Y += (int)velo[i].Y;
+                }
+                for (int i = 0; i < 4; i++)
+                {
+                    List<Rectangle> currec = levellistrectangles[level];
+                    for (int j = 0; j < currec.Count(); j++)
+                    {
+                        if (player[i].Intersects(currec[j]))
+                        {
+                            Rectangle playerbx = new Rectangle((int)(player[i].X + velo[i].X), player[i].Y, player[i].Width, player[i].Height); // player before x
+                            Rectangle playerby = new Rectangle(player[i].X, (int)(player[i].Y + velo[i].Y), player[i].Width, player[i].Height); // player before y
+                            if (playerby.Intersects(currec[j]))
+                            {
+                                velo[i].Y *= -1;
+                            }
+                            else if (playerbx.Intersects(currec[j]))
+                            {
+                                velo[i].X *= -1;
+                            }
+                            else
+                            {
+                                velo[i] *= -1;
+                            }
+                        }
+                    }
+                }
             }
             void InitializeLevel()
             {
-                for (int i = 0; i < startingpos[1].Count(); i++)
+                for (int i = 0; i < startingpos[level].Count(); i++) // TODO fix level int so it represents level, didn't work when i had [level]
                 {
-                    var pos = startingpos[1].ElementAt(i).Value;
+                    var pos = startingpos[level].ElementAt(i).Value;
                     Rectangle playerrec = new Rectangle((int)pos.X, (int)pos.Y, 15, 15);
                     player[i] = playerrec;
+                }
+                for (int i = 0; i < 4; i++)
+                {
+                    Random rng = new Random();
+                    if (rng.Next(2) == 1)
+                    {
+                        velo[i].X = 1;
+                    }
+                    else
+                    {
+                        velo[i].X = -1;
+                    }
+                    if (rng.Next(2) == 1)
+                    {
+                        velo[i].Y = 1;
+                    }
+                    else
+                    {
+                        velo[i].Y = -1;
+                    }
                 }
             }
             base.Update(gameTime);
@@ -193,9 +251,9 @@ namespace Spel_Projekt_Thor_Grimes
             spriteBatch.Begin();
             if (gamestate == 0)
             {
-                spriteBatch.DrawString(MMFont, "Placeholder", new Vector2(325, 100), Color.White);
+                spriteBatch.DrawString(MMFont, "Placeholder", new Vector2(330, 95), Color.White);
                 spriteBatch.Draw(Button, new Vector2(300, 200), Color.White * globalhoverdarken);
-                spriteBatch.DrawString(MMFont, "PLAY!", new Vector2(370, 225), Color.DeepSkyBlue * globalhoverdarken);
+                spriteBatch.DrawString(MMFont, "PLAY!", new Vector2(370, 220), Color.DeepSkyBlue * globalhoverdarken);
             }
             else if (gamestate == 1)
             {
@@ -212,14 +270,15 @@ namespace Spel_Projekt_Thor_Grimes
                 {
                     spriteBatch.Draw(basic, player[i], Color.White);
                 }
+                List<Rectangle> currec = levellistrectangles[level];
+                for (int i = 0; i < currec.Count(); i++)
+                {
+                    spriteBatch.Draw(basic, currec[i], Color.White);
+                }
             }
             foreach (var item in rectangles)
             {
                 spriteBatch.Draw(basic, item, Color.White);
-            }
-            if (rectangles.Count() != 0)
-            {
-                spriteBatch.DrawString(MMFont, gamestate.ToString(), new Vector2(400, 10), Color.White);
             }
             spriteBatch.End();
             base.Draw(gameTime);
